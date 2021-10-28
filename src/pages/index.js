e16bf3adbd57b1b6da30db883da30fda79c5bca0
Shumepi <shumpei.o.7g@gmail.com>
2021-10-29 02:23:44 +0900

    Update Projectのコンテンツ追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import Layout from '../components/layout';
 import FirstView from '../components/first-view';
+import Projects from '../components/projects';
+
 
 const Index = () => {
 	return(
 		<Layout>
 			<FirstView />
+			<Projects />
 		</Layout>
 	)
 };
