afc06c7b64b36850ac1d3af5d859006bf6557d98
Shumepi <shumpei.o.7g@gmail.com>
2021-10-30 02:09:45 +0900

    fix typo

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import * as style from '../styles/module/_footer.module.scss';
 const Footer = () => {
     return (
         <footer className={style.footerContainer}>
-            <div class={style.footerTopBorder} />
+            <div className={style.footerTopBorder} />
             <ul className={style.footerLinks}>
                 <li className={style.footerLinks__item}>Blog</li>
                 <li className={style.footerLinks__item}>About</li>
