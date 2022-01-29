848e76af38e2638eaa1a74371374fe803f49f0ab
Shumpei <shumpei.o.7g@gmail.com>
2022-01-30 03:30:29 +0900

    Add useTransitionFix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import CommonHead from '../components/head/CommonHead';
 import { AnimatePresence } from 'framer-motion';
 
+import { useTransitionFix } from '../lib/useTransitionFix';
+
 import '../styles/base.scss';
 
 function MyApp({ Component, pageProps, router }) {
+	const transitionCallBack = useTransitionFix();
+
 	return (
 		<>
 			<CommonHead />
-			<AnimatePresence exitBeforeEnter>
+			<AnimatePresence exitBeforeEnter onExitComplete={transitionCallBack}>
 				<Component {...pageProps} key={router.route} />
 			</AnimatePresence>
 		</>
